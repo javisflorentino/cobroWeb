f43d5df7838b47eebff40a89597769853be6380f
canr0906 <cnazarior@gmail.com>
2023-10-09 17:06:54 -0600

    Sigo con problemas en _HttpClient

--- src/app/shared/services/smy-calculo-pagos.service.ts
+++ src/app/shared/services/smy-calculo-pagos.service.ts
@@ -10,25 +10,26 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class SmyCalculoPagosService {
 
-  private urlSmytParticular = 'https://app.hacienda.morelos.gob.mx/serviciosHacienda/smyt/particular';
+  private urlSmytParticular = 'serviciosHacienda/smyt/particular';
+  //'https://app.hacienda.morelos.gob.mx/serviciosHacienda/smyt/particular';
 
   constructor(private http: HttpClient, private activetedRouter: ActivatedRoute ) { }
 
   getCalculoPagos(datosTramite:DatosTramite): Observable<TopLevel[]> {
 
-    let headers_object = new HttpHeaders();
+    const headers = new HttpHeaders();
 
-    headers_object.append('Access-Control-Allow-Origin', '*');
-    headers_object.append("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
-   headers_object.append('Access-Control-Allow-Methods',"POST");
-    headers_object.append('Content-Type', 'application/json');
+    //headers_object.append('Access-Control-Allow-Origin', 'Origin, X-Requested-With, Content-Type, Accept');
+    //headers_object.append("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
+    //headers_object.append('Access-Control-Allow-Methods',"POST");
+    headers.append('Content-Type', 'application/json; charset=utf-8');
     //this.headers_object.append('Accept','application/json');
-    headers_object.append("Authorization", "Basic " + btoa("WS_SH1:Hdes22G*_106"));
+    headers.set("Authorization", "Basic " + btoa("WS_SH1:Hdes22G*_106"));
 
 
 
     const httpOptions = {
-      headers: headers_object
+      headers: headers
     };
 
 
@@ -36,12 +37,12 @@ export class SmyCalculoPagosService {
 
     const requestOptions = {
       method: 'POST',
-      headers: headers_object,
+      headers: headers,
       body: JSON.stringify(updateData),
       redirect: 'follow'
     };
 
-    console.log("smyt_calculo-pago-service");
+    console.log("smyt_calculo-pago-service" + requestOptions.body);
     return this.http.post<TopLevel[]>(`${this.urlSmytParticular}`,requestOptions)
       .pipe(
         tap(arrTramite => console.log(arrTramite)),
