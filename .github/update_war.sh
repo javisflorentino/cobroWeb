da9cfe18f507580f276687fbe396bc115940a3d6
Jorge Mendoza <69874204+jorgeLTS@users.noreply.github.com>
2023-12-07 11:57:55 -0600

    Update update_war.sh

--- .github/update_war.sh
+++ .github/update_war.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-LOCAL=/home/jenkins/jenkins_pipeline/workspace/QA_portalpagoservicios
-DEPLOY=/home/docker/9200_qa.hacienda.morelos.gob.mx/9200_portalpagoservicios
+LOCAL=/home/jenkins/jenkins_pipeline/workspace/Pro_portalpagoservicios
+DEPLOY=/home/docker/9000_app.hacienda.morelos.gob.mx/9030_portalpagoservicios
 echo "Borrando Deploy Pasado"
 rm -rf $DEPLOY/data/portalpagoservicios/*
 sleep 10
