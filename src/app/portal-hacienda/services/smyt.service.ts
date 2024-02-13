573c39c0778f98497d60bf071e58643e78be8f35
canr0906 <cnazarior@gmail.com>
2024-02-13 14:03:20 -0600

    ...

--- src/app/portal-hacienda/services/smyt.service.ts
+++ src/app/portal-hacienda/services/smyt.service.ts
@@ -80,7 +80,7 @@ export class SmytService {
          </smyt:obtenEstatusVehiculo>
       </soapenv:Body>
    </soapenv:Envelope>`,
-      headers: { "Content-type": "text/xml; charset=utf-8", "http-equiv":"Content-Security-Policy", "content":"upgrade-insecure-requests" }
+      headers: { "Content-type": "text/xml; charset=utf-8", "Content-Security-Policy": "upgrade-insecure-requests" }
     })
   }
 
