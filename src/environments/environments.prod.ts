dc304802e78b3924582e02b5feaea3093cf83f18
canr0906 <cnazarior@gmail.com>
2024-02-22 11:45:50 -0600

    ...

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -1,6 +1,6 @@
 export const environments = {
   baseUrlApp: 'https://app.hacienda.morelos.gob.mx/',
-  baseUrlServ: 'https://servicioshacienda.morelos.gob.mx:8080/',
+  baseUrlServ: 'https://app.hacienda.morelos.gob.mx/',//'https://servicioshacienda.morelos.gob.mx:8080/',
   user_server: 'WS_SH1',
   pass_server: 'Hdes22G*_106',
   valor_uma: 104
