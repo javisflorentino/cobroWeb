11a2159bc7bc8e387164e4443f3095abffaaee76
Ernesto Márquez Cardoso <shm.ernesto.mc.84@gmail.com>
2024-10-23 13:45:28 -0600

    actualizacion

---
+++ .github/update_node_105.120.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+LOCAL=/home/jenkins/jenkins_pipeline/workspace/Pro_portalpagoservicios
+DEPLOY=/home/docker/9000_app.hacienda.morelos.gob.mx/9430_portalpagoservicios
+echo "Borrando Deploy Pasado"
+rm -rf $DEPLOY/data/portalpagoservicios/*
+sleep 10
+#
+echo "Deteniendo Servicio en Nodo"
+cd $DEPLOY
+docker compose down
+sleep 10
+#
+echo "Creando Servicio en Nodo"
+mv $LOCAL/deploy/* $DEPLOY/data/portalpagoservicios/
+#
+echo "Iniciando Servicio"
+cd $DEPLOY
+docker compose up -d
+sleep 8
