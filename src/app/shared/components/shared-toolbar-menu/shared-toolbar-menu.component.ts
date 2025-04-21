df94e5a743a1703d413b1c7a741c44759cb602aa
canr0906 <cnazarior@gmail.com>
2025-04-21 16:51:39 -0600

    Se modificó el menú de pagos en línea. Se agrego un toolbar, se modificó layout-portal-pagos y cards-dependencias

--- src/app/shared/components/shared-toolbar-menu/shared-toolbar-menu.component.ts
+++ src/app/shared/components/shared-toolbar-menu/shared-toolbar-menu.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+
+import { ModalComprobantePagoComponent } from '../modal-comprobante-pago/modal-comprobante-pago.component';
+import { ModalPagoLineaComponent } from '../modal-pago-linea/modal-pago-linea.component';
+import { ModalHistoricoPagosComponent } from '../modal-historico-pagos/modal-historico-pagos.component';
+import { ModalFacturacionComponent } from '../modal-facturacion/modal-facturacion.component';
 
 @Component({
   selector: 'shared-toolbar-menu',
@@ -7,4 +13,71 @@ import { Component } from '@angular/core';
 })
 export class SharedToolbarMenuComponent {
 
+  show = false;
+
+  menuButtons = [
+    { icon: 'receipt_long', label: 'Recibo de Pago' },
+    { icon: 'request_quote', label: 'Facturación' },
+    { icon: 'history', label: 'Histórico' },
+    { icon: 'payments', label: 'Pagar Póliza' }
+  ];
+
+  constructor(private dialog: MatDialog){}
+
+  openDialog(button: any): void {
+    // Solo abrimos el diálogo si es el botón de "Pagar Póliza"
+    if (button.label === 'Pagar Póliza') {
+      const dialogRef = this.dialog.open(ModalPagoLineaComponent, {
+        width: '350px',
+        disableClose: false,
+
+      });
+
+      dialogRef.afterClosed().subscribe(result => {
+        if (result) {
+          console.log('Datos del formulario:', result);
+          // Aquí puedes manejar los datos recibidos del diálogo
+        }
+      });
+    }
+    if (button.label === 'Recibo de Pago') {
+      const dialogRef = this.dialog.open(ModalComprobantePagoComponent, {
+        width: '350px',
+        disableClose: false
+      });
+
+      dialogRef.afterClosed().subscribe(result => {
+        if (result) {
+          console.log('Datos del formulario:', result);
+          // Aquí puedes manejar los datos recibidos del diálogo
+        }
+      });
+    }
+    if (button.label === 'Histórico') {
+      const dialogRef = this.dialog.open(ModalHistoricoPagosComponent, {
+        width: '350px',
+        disableClose: false
+      });
+
+      dialogRef.afterClosed().subscribe(result => {
+        if (result) {
+          console.log('Datos del formulario:', result);
+          // Aquí puedes manejar los datos recibidos del diálogo
+        }
+      });
+    }
+    if (button.label === 'Facturación') {
+      const dialogRef = this.dialog.open(ModalFacturacionComponent, {
+        width: '550px',
+        disableClose: false
+      });
+
+      dialogRef.afterClosed().subscribe(result => {
+        if (result) {
+          console.log('Datos del formulario:', result);
+          // Aquí puedes manejar los datos recibidos del diálogo
+        }
+      });
+    }
+  }
 }
