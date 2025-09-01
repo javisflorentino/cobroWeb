7b804eef02ef21fd225ae9178f375de50cb62d1e
canr0906 <cnazarior@gmail.com>
2025-09-01 09:56:44 -0600

    Proxy y environment se le agregaron las rutas de QA

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -4,8 +4,8 @@ export const environments = {
   user_server: 'WS_SH1',
   pass_server: 'Hdes22G*_106',
   valor_uma: 104,
-  //URL_PAGO_EN_LINEA: 'http://localhost:8080/pagoLineaQA',
-  URL_PAGO_EN_LINEA: 'https://app.hacienda.morelos.gob.mx/pagoenlinea',
-  URL_PAGO_EN_LINEA_RECIBO: 'https://app.hacienda.morelos.gob.mx/recibo'
+  //URL_PAGO_EN_LINEA: 'http://localhost:8080/pagoenlinea',
+  URL_PAGO_EN_LINEA: 'https://app.hacienda.morelos.gob.mx/pagoLineaQA',//'https://app.hacienda.morelos.gob.mx/pagoenlinea',
+  URL_PAGO_EN_LINEA_RECIBO: 'https://qa.hacienda.morelos.gob.mx/recibo'
 }
 
