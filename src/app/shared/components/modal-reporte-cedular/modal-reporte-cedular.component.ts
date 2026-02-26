9ac536e4ee2d3dcd25e6da47c4fde3f5d6d8e2fa
canr0906 <cnazarior@gmail.com>
2026-02-26 16:38:12 -0600

    Componente Cedular se agrego la peticion a login de serviciosHacienda para generar el Token

--- src/app/shared/components/modal-reporte-cedular/modal-reporte-cedular.component.ts
+++ src/app/shared/components/modal-reporte-cedular/modal-reporte-cedular.component.ts
@@ -1,18 +1,21 @@
-import { Component, Inject } from '@angular/core';
+import { Component, inject, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { ImpuestoCedularService, ReporteCedularRequest } from 'src/app/shared/services/impuesto-cedular.service';
+import { AuthSiigemService } from '../../services/auth-siigem.service';
 
 @Component({
   selector: 'app-modal-reporte-cedular',
   templateUrl: './modal-reporte-cedular.component.html',
   styleUrls: ['./modal-reporte-cedular.component.css']
 })
-export class ModalReporteCedularComponent {
-  
+export class ModalReporteCedularComponent implements OnInit {
+
   reporteForm: FormGroup;
   isLoading = false;
   errorMessage = '';
+
+  private authSiigemService = inject(AuthSiigemService);
 
   constructor(
     private fb: FormBuilder,
@@ -26,6 +29,12 @@ export class ModalReporteCedularComponent {
       lineaCaptura: [''],
     });
   }
+  ngOnInit(): void {
+    this.authSiigemService.login().subscribe({
+      next: (res) => console.log('Login exitoso', res),
+      error: (err) => console.error('Error al autenticar', err)
+    });
+  }
 
   generarReporte(): void {
     if (this.reporteForm.valid) {
@@ -37,27 +46,27 @@ export class ModalReporteCedularComponent {
       this.impuestoCedularService.generarReporte(request).subscribe({
         next: (response) => {
           this.isLoading = false;
-          
+
           // Obtener el nombre del archivo
           const contentDisposition = response.headers.get('Content-Disposition');
           const filename = this.impuestoCedularService.extraerNombreArchivo(
-            contentDisposition, 
-            request.serie, 
+            contentDisposition,
+            request.serie,
             request.folio
           );
 
           // Descargar el archivo
           this.impuestoCedularService.descargarArchivo(response.body as Blob, filename);
-          
+
           this.dialogRef.close();
         },
         error: (error) => {
           this.isLoading = false;
           console.error('Error al generar reporte:', error);
-          
- 
-            this.errorMessage = 'No se encontró registro del recibo ingresado, favor de verificar';
-          
+
+
+          this.errorMessage = 'No se encontró registro del recibo ingresado, favor de verificar';
+
         }
       });
     }
